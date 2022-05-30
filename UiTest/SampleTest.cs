d5458600d4226eddc8ad9b9225ac484eb7d0b12e
Reza <reza.mansouri70@gmail.com>
2022-05-30 19:32:41 +0430

    Add project files.

---
+++ UiTest/SampleTest.cs
@@ -0,0 +1,20 @@
+using Xunit;
+
+namespace UiTest
+{
+    public class SampleTest
+    {
+        [Fact]
+        public void Just_For_Run_Sample_Test()
+        {
+            // Arrange
+            var myvar = "test";
+
+            //Act
+            bool result= (myvar == "test");
+
+            //Assert 
+            Assert.True(result); 
+        }
+    }
+}
